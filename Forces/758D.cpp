e78371fd650ddfc747cbcebb7358e31e7c321276
FundamentalEq <bloodspain@gmail.com>
2017-02-03 19:21:29 +0530

    codeforces 758D

--- Forces/758D.cpp
+++ Forces/758D.cpp
@@ -12,23 +12,35 @@
 #define ll long long
 #define db long double
 using namespace std ;
+string K ;
+ll fval(int l,int r)
+{
+	ll ans = 0 ;
+	l = max(0,l+1) ;
+	while(l<=r) ans = ans*10 + K[l++] - '0' ;
+	return ans ;
+}
+int flen(int x) { int ans = 0 ; while(x) ++ans,x/=10 ; return ans ;}
 int main()
 {
 	std::ios::sync_with_stdio(false);
-	ll N,cur = 0,mul = 1 , ans = 0; string K ; cin>>N>>K ;
-	ll PW[10] ; PW[0] = 1 ; FEN(i,10) PW[i] = PW[i-1] * 10 ;
-	int s = 0 ;
+	ll N,mul = 1 , ans = 0; cin>>N>>K ;
+	int len = flen(N) ;
+	int l = sz(K)-2,r = sz(K)-1,d ;
 	for(int i = sz(K) - 1 ; i>=0 ; --i)
 	{
-		if( ( cur + PW[s]*max(1,( K[i] - '0')) )< N ) cur = ( cur + PW[s]*( K[i] - '0') ) , ++s;
+		if( fval(l-1,r) < N && r - l + 1 <= len ) --l ;
 		else
 		{
-			ans += cur*mul ;
+			d = l ;
+			while(K[d+1] == '0' && r-d>1) ++d ;
+			ans += fval(d,r)*mul ;
 			mul *= N ;
-			cur = K[i] - '0' , s = 1;
+			r = d ;
+			++i ;
 		}
 	}
-	if(s > 0) ans += cur*mul ;
+	if(l<r) ans += fval(-1,r)*mul ;
 	cout<<ans<<endl ;
 	return 0 ;
 }
